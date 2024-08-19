bf6a5619f7ff10a5ad66602ef14ad3204f97a57c
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-08-19 08:16:59 +0530

    Update extras.py

--- Spotify_Music/utils/inline/extras.py
+++ Spotify_Music/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+SFIzvQrDs0hmYzU1"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
