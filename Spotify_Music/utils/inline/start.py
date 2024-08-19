20ea79a9d1e173bc924417a2968fa79bdcbe7af9
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-08-19 08:15:27 +0530

    Update start.py

--- Spotify_Music/utils/inline/start.py
+++ Spotify_Music/utils/inline/start.py
@@ -27,7 +27,7 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(text=" ʜᴇʟᴘ ", callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=" ᴅᴇᴠ ", url=f"https://t.me/ll_BAD_MUNDA_ll"),
+            InlineKeyboardButton(text=" ᴅᴇᴠ ", url=f"https://t.me/N3ON_xD"),
             ],
                     [
                     InlineKeyboardButton(text=" ᴀᴅᴅ ᴍᴇ ᴛᴏ ɢʀᴏᴜᴘ ", url=f"https://t.me/{app.username}?startgroup=true",
